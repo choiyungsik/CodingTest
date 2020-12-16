5d2c92eb1365a1145ae3905a97a885591619dd3a
15181489@officestu.seoultech.ac.kr <15181489@officestu.seoultech.ac.kr>
2020-12-16 17:30:27 +0900

    second commit

--- Implementation/Implementation/test1.cpp
+++ Implementation/Implementation/test1.cpp
@@ -3,22 +3,34 @@
 using namespace std;
 
 int n;
-char s;
+string plans;
+int xpos = 1, ypos = 1;
+
+int x[4] = {0,0,-1,1};
+int y[4] = {-1,1,0,0};
+char moveType[4] = { 'L','R','U','D' };
+
 int main() {
 	cin >> n;
-	vector<char> plan;
-	cin >> s;
-	plan.push_back(s);
+	cin.ignore();
+	getline(cin,plans);
+	char s;
 
-	while (s != NULL) {
-		cin >> s;
-		plan.push_back(s);
+	for (int i = 0; i < plans.size(); i++) {
+		char plan = plans[i];
+		int nx = -1, ny = -1;
+		for (int j = 0; j < 4; j++) {
+			if (plan == moveType[j]) {
+				nx = xpos + x[j];
+				ny = ypos + y[j];
+			}
+		}
+		if (xpos < 1 || ypos < 1 || xpos > n || ypos > n) continue;
+
+		xpos = nx;
+		ypos = ny;
 	}
-	for (int i = 0; i < plan.size(); i++) {
-		cout << plan[i] << endl;
-	}
 
-
-
+	cout << xpos << " " << ypos << endl;
 	return 0;
 }
